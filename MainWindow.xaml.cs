7fdf7444286932258385730c4171b8fb366830b8
Robert <rgmerski@gmail.com>
2023-10-05 14:47:08 +0200

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> tasks = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            taskListBox.ItemsSource = tasks;
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            string task = taskTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(task))
+            {
+                tasks.Add(task);
+                taskTextBox.Clear();
+            }
+        }
+
+        private void RemoveTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (taskListBox.SelectedIndex != -1)
+            {
+                int selectedIndex = taskListBox.SelectedIndex;
+                tasks.RemoveAt(selectedIndex);
+            }
+        }
+
+        private void CompleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (taskListBox.SelectedIndex != -1)
+            {
+                int selectedIndex = taskListBox.SelectedIndex;
+                tasks[selectedIndex] = "[Completed] " + tasks[selectedIndex];
+            }
+        }
+
+        
+    }
+}
---
+++ Model/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Model
+{
+    internal class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTime Deadline { get; set; }
+        public int Priority { get; set; }
+    }
+}
